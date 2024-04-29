43809f2b14a6022b07fca52fa6462233ac400a82
Habib Ogunsola <ogunsolahabib@yahoo.com>
2024-04-29 14:07:55 +0100

    sticky headers

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 import ThemeSwitcher from "../ui/ThemeSwitcher";
 
 export default function Header() {
-    return <header className="header bg-base-100 sticky inset-0">
+    return <header className="navbar bg-base-100 sticky inset-0  z-20 border-b border-b-base-300">
         <div className="container mx-auto">
-            <div className="flex justify-between items-center">
+            <div className="flex justify-between items-center w-full ">
                 <Link to="/"><span className="p-4">App Logo</span></Link>
                 <ThemeSwitcher />
             </div>
