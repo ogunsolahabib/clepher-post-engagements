6212e9884a15aa7fe678afb73ce09cdbfc14502b
Habib Ogunsola <ogunsolahabib@yahoo.com>
2024-04-29 11:33:17 +0100

    custom hook for pagination

---
+++ src/custom-hooks/usePagination.ts
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+
+const usePagination = (totalPages: number, initialPage = 1, pageSize = 1) => {
+    const [currentPage, setCurrentPage] = useState(initialPage);
+    const [startIndex, setStartIndex] = useState((initialPage - 1) * pageSize);
+    const [endIndex, setEndIndex] = useState(startIndex + pageSize - 1);
+
+    useEffect(() => {
+        setStartIndex((currentPage - 1) * pageSize);
+        setEndIndex(startIndex + pageSize - 1);
+    }, [currentPage, pageSize, startIndex]);
+
+    const handlePageChange = (pageNumber: number) => {
+        if (pageNumber >= 1 && pageNumber <= totalPages) {
+            setCurrentPage(pageNumber);
+        }
+    };
+
+    const hasPreviousPage = currentPage > 1;
+    const hasNextPage = currentPage < totalPages;
+
+    return {
+        currentPage,
+        startIndex,
+        endIndex,
+        handlePageChange,
+        hasPreviousPage,
+        hasNextPage,
+    };
+};
+
+export default usePagination;
